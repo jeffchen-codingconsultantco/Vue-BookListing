59a8083c7e9767ea962dfab507348dd8663e2d4a
Jeff Chen <yetchen2009@gmail.com>
2019-09-21 23:01:34 +1000

    Complete module

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,17 +1,17 @@
 <template>
-  <li>{{ book.title }}:{{book.author}}</li>
+    <li>{{ book.title }}:{{book.author}}</li>
 </template>
 
 <script>
 export default {
-  name: "BookItem",
-  props: ["book"]
+    name: "BookItem",
+    props: ["book"]
 };
 </script>
 
 <style scoped>
 li {
-  display: block;
-  margin: 0 10px;
+    display: block;
+    margin: 0 10px;
 }
 </style>
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,43 +1,54 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ul>
-      <li v-for="book in books">
-        <book-item :book="book" />
-      </li>
-    </ul>
-  </div>
+    <div>
+        <h1>{{ title }}</h1>
+        <ul>
+            <li v-for="book in books">
+                <book-item :book="book" />
+            </li>
+        </ul>
+        <book-form @addBook="appendBook" />
+    </div>
 </template>
 
 <script>
 import BookItem from "./BookItem";
+import BookForm from "./BookForm";
 
 export default {
-  name: "BookList",
-  components: {
-    BookItem
-  },
-  data() {
-    return {
-      title: "All Books",
-      books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        { title: "American Gods", author: "Neil Gaiman" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
-      ]
-    };
-  }
+    name: "BookList",
+    components: {
+        BookItem,
+        BookForm
+    },
+    data() {
+        return {
+            title: "All Books",
+            books: [
+                { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+                { title: "American Gods", author: "Neil Gaiman" },
+                { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+            ]
+        };
+    },
+    methods: {
+        appendBook(bookTitle, bookAuthor) {
+            this.books.push({
+                title: bookTitle,
+                author: bookAuthor
+            });
+        }
+    }
 };
 </script>
 
 <style>
 h1,
 h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 </style>
